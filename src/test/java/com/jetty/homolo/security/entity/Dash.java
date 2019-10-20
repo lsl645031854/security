81d321d47b04d6be1951d5e0b8762e78c4840cc6
lsl645031854 <645031854@qq.com>
2019-10-20 22:18:09 +0800

    stream api

--- src/test/java/com/jetty/homolo/security/entity/Dash.java
+++ src/test/java/com/jetty/homolo/security/entity/Dash.java
@@ -1,15 +1,15 @@
 package com.jetty.homolo.security.entity;
 
+
 /**
  * @Author: lsl
  * @Description:
  * @Date: Created on 14:31 2019/10/16
  */
-public class Dash {
+public class Dash implements Comparable<Dash>{
     private String name; // 菜名
     private Double price; // 价格
     private Integer calories; // 卡路里
-
     public Dash() {
     }
 
@@ -44,7 +44,32 @@ public class Dash {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dash)) return false;
+
+        Dash dash = (Dash) o;
+
+        if (!name.equals(dash.name)) return false;
+        if (!price.equals(dash.price)) return false;
+        return calories.equals(dash.calories);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + price.hashCode();
+        result = 31 * result + calories.hashCode();
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "Dash{" + "name='" + name + '\'' + ", price=" + price + ", calories=" + calories + '}';
+    }
+
+    @Override
+    public int compareTo(Dash dash) {
+        return -(this.getCalories() - dash.getCalories());
     }
 }
--- src/test/java/com/jetty/homolo/security/stream/StreamTest.java
+++ src/test/java/com/jetty/homolo/security/stream/StreamTest.java
@@ -3,13 +3,16 @@ package com.jetty.homolo.security.stream;
 import com.jetty.homolo.security.entity.Dash;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * @Author: lsl
- * @Description:
+ * @Description: stream Api
  * @Date: Created on 21:33 2019/10/16
  */
 public class StreamTest {
@@ -17,9 +20,13 @@ public class StreamTest {
             new Dash("番茄鸡蛋", 12.3, 400),
             new Dash("青椒鸡蛋", 10.8, 550),
             new Dash("苦瓜鸡蛋", 15.3, 510),
+            new Dash("香菜鸡蛋", 15.3, 510),
+            new Dash("黄瓜鸡蛋", 18.3, 600),
+            new Dash("黄瓜鸡蛋", 18.3, 600),
             new Dash("黄瓜鸡蛋", 18.3, 600)
     );
 
+    // 创建stream
     @Test
     public void test() {
         Stream<Dash> stream = dashes.stream();
@@ -28,8 +35,99 @@ public class StreamTest {
         Stream<Dash> stream1 = Arrays.stream(array);
 
         Stream<String> stringStream = Stream.of("aa", "bb", "cc");
-
     }
 
+    // 中间操作
+    @Test
+    public void test1() {
+        // 筛选切片 filter(Predicate p)——接收 Lambda ， 从流中排除某些元素。
+        Stream<Dash> dashStream = dashes.stream().filter(dash -> dash.getCalories() > 500);
+        dashStream.forEach(System.out::println);
+    }
+    @Test
+    public void test2() {
+        // 筛选切片
+        Stream<Dash> dashStream = dashes.stream().filter(dash -> dash.getCalories() > 500);
+        dashStream.skip(1).limit(1).forEach(System.out::println);
+    }
+    @Test
+    public void test3() {
+        // distinct()——筛选去重，通过流所生成元素的 hashCode() 和 equals() 去除重复元素
+        Stream<Dash> dashStream = dashes.stream().filter(dash -> dash.getCalories() == 600);
+        dashStream.distinct().forEach(System.out::println);
+    }
 
+    @Test
+    public void test4() {
+//      map(Function f)——接收 Lambda，将元素转换成其他形式或提取信息。接收一个函数作为参数，
+//      该函数会被应用到每个元素上，并将其映射成一个新的元素。
+//      <R> Stream<R> flatMap(Function<? super T, ? extends Stream<? extends R>> mapper);
+//      <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+        dashes.stream().distinct().map(Dash::getName).forEach(System.out::println);
+
+        List<List<String>> lists = Arrays.asList(Arrays.asList("a", "b"), Arrays.asList("b","c", "d"));
+        Function<List<String>, Stream<String>> function = list -> list.stream();
+        Stream<String> stringStream = lists.stream().flatMap(Collection::stream);
+        stringStream.forEach(System.out::println);
+    }
+    @Test
+    public void test5() {
+      // sorted()——自然排序
+      // sorted(Comparator com)——定制排序
+      dashes.stream().distinct().sorted().forEach(System.out::println);
+      System.out.println("------------------------------");
+      dashes.stream().sorted((Dash d1, Dash d2) -> {
+            if (d1.getCalories().equals(d2.getCalories())) {
+                return d1.getName().compareTo(d2.getName()) ;
+            } else {
+                return d1.getCalories().compareTo(d2.getCalories());
+            }
+        }).forEach(System.out::println);
+    }
+    // 终止操作
+//    1- 查找与匹配
+//allMatch(Predicate p)——检查是否匹配所有元素
+//anyMatch(Predicate p)——检查是否至少匹配一个元素
+//noneMatch(Predicate p)——检查是否没有匹配的元素
+//findFirst——返回第一个元素
+//findAny——返回当前流中的任意元素
+//count——返回流中元素的总个数
+//max(Comparator c)——返回流中最大值
+//min(Comparator c)——返回流中最小值
+//forEach(Consumer c)——内部迭代
+    @Test
+    public void test6() {
+        boolean b = dashes.stream().anyMatch(dash -> dash.getCalories() == 510);
+        System.out.println(b);
+        boolean b1 = dashes.stream().allMatch(dash -> !dash.getName().equals(""));
+        System.out.println(b1);
+        Optional<Integer> max = dashes.stream().map(Dash::getCalories).max(Integer::compare);
+        System.out.println(max.get());
+
+        Optional<Dash> min = dashes.stream().min(Comparator.comparingInt(Dash::getCalories));
+        System.out.println(min.get());
+    }
+//    归约
+//    reduce(T identity, BinaryOperator)——可以将流中元素反复结合起来，得到一个值。返回 T
+//    reduce(BinaryOperator) ——可以将流中元素反复结合起来，得到一个值。返回 Optional<T>
+    @Test
+    public void test7() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        Optional<Integer> reduce = list.stream().reduce((x, y) -> x + y);
+        System.out.println(reduce.get());
+
+        Integer reduce1 = list.stream().reduce(0, (x, y) -> x + y);
+        System.out.println(reduce1);
+        System.out.println("==========================");
+        Optional<Integer> sum = dashes.stream().map(Dash::getCalories).reduce(Integer::sum);
+    }
+//  3-收集
+//  collect(Collector c)——将流转换为其他形式。接收一个 Collector接口的实现，
+//                      用于给Stream中元素做汇总的方法
+    @Test
+    public void test8() {
+        List<Integer> list = dashes.stream().map(Dash::getCalories).collect(Collectors.toCollection(ArrayList::new));
+        int sum = dashes.stream().mapToInt(Dash::getCalories).sum();
+        long count = dashes.stream().map(Dash::getCalories).count();
+    }
 }
