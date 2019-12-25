d19f046fc89927c8a9cbd6c203eea554f13f6f5b
李帅领 <lishuailing@homolo.com>
2019-12-25 18:02:21 +0800

    http

---
+++ src/test/java/com/jetty/homolo/security/http/HttpClientUtils.java
@@ -0,0 +1,155 @@
+package com.jetty.homolo.security.http;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author homolo
+ * @DESC lsl
+ * @Create 2019-12-25  下午5:11
+ */
+public class HttpClientUtils {
+	public static String doGet(String url, Map<String, String> params) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		CloseableHttpResponse response = null;
+		String resultString = "";
+		try {
+			URIBuilder builder = new URIBuilder(url);
+			if (params != null) {
+				for (Map.Entry entry : params.entrySet()) {
+					builder.addParameter(entry.getKey().toString(), entry.getValue().toString());
+				}
+			}
+			URI uri = builder.build();
+
+			HttpGet httpGet = new HttpGet(uri);
+//			httpGet.addHeader("Content-Type", "application/json;charset=utf-8");
+			response = httpClient.execute(httpGet);
+			resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+			System.out.println(resultString);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultString;
+	}
+
+	public static String doPost(String url, Map<String, String> param) {
+		// 创建Httpclient对象
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		CloseableHttpResponse response = null;
+		String resultString = "";
+		try {
+			// 创建Http Post请求
+			HttpPost httpPost = new HttpPost(url);
+			// 创建参数列表
+			if (param != null) {
+				List<NameValuePair> paramList = new ArrayList<>();
+				for (String key : param.keySet()) {
+					paramList.add(new BasicNameValuePair(key, param.get(key)));
+				}
+				// 模拟表单
+				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList);
+				httpPost.setEntity(entity);
+			}
+//			httpPost.setHeader();
+			// 执行http请求
+			response = httpClient.execute(httpPost);
+			resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return resultString;
+	}
+
+	public static String doPostJson(String url, String json) {
+		// 创建Httpclient对象
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		CloseableHttpResponse response = null;
+		String resultString = "";
+		try {
+			// 创建Http Post请求
+			HttpPost httpPost = new HttpPost(url);
+			// 创建请求内容
+			StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+			httpPost.setEntity(entity);
+			// 执行http请求
+			response = httpClient.execute(httpPost);
+			resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				response.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return resultString;
+	}
+
+	/**
+	 * 模拟文件上传
+	 */
+	public void upload() {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpPost httppost = new HttpPost("http://localhost:8080/myDemo/Ajax/serivceFile.action");
+
+			FileBody bin = new FileBody(new File("F:\\image\\sendpix0.jpg"));
+			StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
+			HttpEntity reqEntity = MultipartEntityBuilder.create().addPart("bin", bin).addPart("comment", comment).build();
+
+			httppost.setEntity(reqEntity);
+
+			System.out.println("executing request " + httppost.getRequestLine());
+			try (CloseableHttpResponse response = httpclient.execute(httppost)) {
+				System.out.println("----------------------------------------");
+				System.out.println(response.getStatusLine());
+				HttpEntity resEntity = response.getEntity();
+				if (resEntity != null) {
+					System.out.println("Response content length: " + resEntity.getContentLength());
+				}
+				EntityUtils.consume(resEntity);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				httpclient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
